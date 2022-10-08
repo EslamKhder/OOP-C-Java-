42851372f1d887fe2012ca83e93faf69a144547a
eslam khder <eslamkhder11@gmail.com>
2022-10-08 12:51:32 +0200

    problem 1

--- c++/1/answer/problem 1.cpp
+++ c++/1/answer/problem 1.cpp
@@ -1,5 +1,20 @@
 #include<iostream>
 using namespace std;
+class Employee {
+	public: 
+		int id;
+		string name;
+		double age;
+};
 int main(){
 	
+	Employee e;
+	cout<<"Please Enter Your Id :";
+	cin>>e.id;
+	cout<<"Please Enter Your Name :";
+	cin>>e.name;
+	cout<<"Please Enter Your Age :";
+	cin>>e.age;
+	
+	cout<<e.id<<" - "<<e.name<<" - "<<e.age<<endl;
 }
