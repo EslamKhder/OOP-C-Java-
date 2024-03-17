2becdfe8f0a6cd4c7c06a7a3e9fc2ec52d9676b0
Eslam.khder <eslamkhder11@gmail.com>
2024-03-17 15:20:17 +0200

    problim Circle

--- c++/29/answer/19.cpp
+++ c++/29/answer/19.cpp
@@ -1,5 +1,36 @@
 #include<iostream>
 using namespace std;
+class Circle {
+	private:
+		double radius;
+		const double BI = 22 / 7;
+	public:
+		
+		Circle(){
+			
+		}
+		Circle(double radius){
+			this->radius = radius;
+		}
+		
+		void setRadius(double radius){
+			this->radius = radius;
+		}
+		
+		double circumference(){
+			return 2 * BI * radius;
+		}
+		double area(){
+			return  BI * radius * radius;
+		}
+};
 int main(){
+	cout<<"Please Enter Your radius"<<endl;
+	double radius;
+	cin>>radius;
 	
+	Circle circle;
+	circle.setRadius(radius);
+	cout<<"circumference = "<<circle.circumference()<<endl;
+	cout<<"area = "<<circle.area()<<endl;
 }
